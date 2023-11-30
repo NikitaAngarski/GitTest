4cedbcd450777189bac467df15571f7ce644e680
NikitaAngarski <nangarski7309@students.mc3.edu>
2023-11-29 19:30:40 -0500

    Added Java file
    This java file prints out hello world and the numbers 1-100, this is a basic program with one main method

---
+++ helloWorld.java
@@ -0,0 +1,11 @@
+public class helloWorld {
+
+   public static void main(String[] args){
+   
+      System.out.println("Hello World");
+      
+      for (int i = 0; i<101; i++){
+         System.out.println(i);
+         }
+      }
+   }
